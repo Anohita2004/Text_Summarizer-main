f95b870e198aa13b1fa450b272e147033470ca30
Anohita2004 <anohitamukherjee@gmail.com>
2024-12-18 12:54:50 +0530

    All the data ingestion changes have been commited

--- main.py
+++ main.py
@@ -4,9 +4,16 @@ import os
 # Add the src folder to the system path
 sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
 
+from textSummariser.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummariser.logging import logger
 
-# Rest of your code
 
-
-logger.info("Welcome to our custom logging")
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f">>>>>>>>>stage {STAGE_NAME} started <<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<<<<\n\nx==============x")
+except Exception as e:
+    logger.exception(e)
+    raise e
---
+++ src/textSummariser/config/configuration.py
@@ -0,0 +1,61 @@
+'''import sys
+import os
+
+# Add src directory to sys.path
+sys.path.append(os.path.join(os.getcwd(), 'src'))
+
+from textSummariser.constants import *
+from textSummariser.utils.common import read_yaml, create_directories
+from textSummariser.entity import DataIngestionConfig
+class ConfigurationManager:
+    def __init__(self, config_filepath=CONFIG_FILE_PATH, params_filepath=PARAMS_FILE_PATH):
+        # Load the configuration and parameters from YAML files
+        self.config = read_yaml(config_filepath)
+        self.params = read_yaml(params_filepath)
+        
+        # Create necessary directories
+        create_directories([self.config.artifacts_root])
+
+        # Access the 'data_ingestion' section from the config
+        data_ingestion_section = self.config.get('data_ingestion', {})
+
+        # Create the DataIngestionConfig object with the configuration data
+        self.data_ingestion_config = DataIngestionConfig(config=data_ingestion_section)
+
+    # Method to get the DataIngestionConfig object
+    def get_data_ingestion_config(self):
+        return self.data_ingestion_config'''
+import sys
+import os
+
+# Add src directory to sys.path
+sys.path.append(os.path.join(os.getcwd(), 'src'))
+
+from textSummariser.constants import *
+from textSummariser.utils.common import read_yaml, create_directories
+from textSummariser.entity import DataIngestionConfig
+
+class ConfigurationManager:
+    def __init__(self, config_filepath=CONFIG_FILE_PATH, params_filepath=PARAMS_FILE_PATH):
+        # Load the configuration and parameters from YAML files
+        self.config = read_yaml(config_filepath)
+        self.params = read_yaml(params_filepath)
+        
+        # Create necessary directories
+        create_directories([self.config.artifacts_root])
+
+        # Access the 'data_ingestion' section from the config
+        data_ingestion_section = self.config.get('data_ingestion', {})
+
+        # Ensure that 'data_ingestion_section' has the expected keys and pass them to DataIngestionConfig
+        self.data_ingestion_config = DataIngestionConfig(
+            root_dir=Path(data_ingestion_section.get('root_dir', 'default_root_dir_path')),
+            source_URL=data_ingestion_section.get('source_URL', 'default_url'),
+            local_data_file=Path(data_ingestion_section.get('local_data_file', 'default_local_data_file_path')),
+            unzip_dir=Path(data_ingestion_section.get('unzip_dir', 'default_unzip_dir_path'))
+        )
+
+    # Method to get the DataIngestionConfig object
+    def get_data_ingestion_config(self):
+        return self.data_ingestion_config
+        
